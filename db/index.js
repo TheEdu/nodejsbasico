8512c4035839bb59834fb4d625021b29f166d938
Eduardo Denis <denise@zarate.gob.ar>
2019-01-11 13:43:25 -0300

    Se modifico la api para integrarla con mongo

--- db/index.js
+++ db/index.js
@@ -1,13 +1,31 @@
-const users = require("../resources/users");
+// const users = require("../resources/users");
+const mongo = require("./connect");
+const { DB_NAME } = require("./config");
 
 module.exports = {
     getUsers: function() {
-        return users;
+      const db = mongo.instance().db(DB_NAME);
+      const users = db.collection("users").find({}).toArray();
+      return users;
+      // return users;
     },
     getUserById: function(id) {
-        return users.filter(user=>user._id===id);
+      const db = mongo.instance().db(DB_NAME);
+      const user = db.collection("users").findOne({
+        _id: id
+      });
+      return user;
+      // return users.filter(user=>user._id===id);
     },
     getUserByAgeRange: function(lower = 0, higher = 99) {
-        return users.filter(user=>user.age >= lower && user.age <= higher);
+      const db = mongo.instance().db(DB_NAME);
+      const users = db.collection("users").find({
+        age: {
+          $gte: +lower,
+          $lte: +higher,
+        }
+      }).toArray();
+      return users;
+        // return users.filter(user=>user.age >= lower && user.age <= higher);
     }
 }
